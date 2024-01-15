fb5e9352bff3a1704c8c1e405b61be5a33936774
Roger Boixader Güell <rboixaderg@gmail.com>
2024-01-15 09:46:07 +0100

    choe: delete vitest from electron app

--- electron-app/vitest.config.ts
+++
@@ -1,7 +0,0 @@
-import tsconfigPaths from 'vite-tsconfig-paths'; // only if you are using custom tsconfig paths
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {},
-  plugins: [tsconfigPaths()], // only if you are using custom tsconfig paths
-});
