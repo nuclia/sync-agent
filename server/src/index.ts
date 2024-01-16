e086dd944b283523bfea0a29e3daa38d3458d629
Roger Boixader Güell <rboixaderg@gmail.com>
2024-01-16 09:06:30 +0100

    chore: start server script

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,17 +5,24 @@ import { syncAllFoldersFileSystemProcess } from './process-sync';
 import { Server, eventEmitter } from './server';
 import { initFileSystemSubscribers } from './subscribers';
 
+type ServerOptions = {
+  secondsForAutoSync?: number;
+  startAutoSyncProcess?: boolean;
+  port?: number;
+};
+
+type FileSystemServerOptions = {
+  basePath: string;
+} & ServerOptions;
+
 export const initFileSystemServer = async ({
   basePath,
   secondsForAutoSync = 3600, // 1h
   startAutoSyncProcess = true,
-}: {
-  basePath: string;
-  secondsForAutoSync?: number;
-  startAutoSyncProcess?: boolean;
-}) => {
+  port = 8080,
+}: FileSystemServerOptions) => {
   const appRoutes = new AppFileSystemRoutes(basePath);
-  const server = new Server({ port: 8000, routes: appRoutes.getRoutes() });
+  const server = new Server({ port, routes: appRoutes.getRoutes() });
   initFileSystemSubscribers(basePath);
   await beforeStartServer(basePath);
 
--- server/src/start-file-system-server.ts
+++ server/src/start-file-system-server.ts
@@ -1,9 +1,23 @@
+import { program } from 'commander';
 import os from 'os';
 import { initFileSystemServer } from '.';
 
 const basePath = `${os.homedir()}/.nuclia`;
+program
+  .option('-das, --disable-auto-sync <boolean>', 'Disable auto sync process', false)
+  .option('-bp, --base-path <string>', 'Path to save the data', basePath)
+  .option('-p, --port <type> <number>', 'Port', '8080')
+  .option('-s, --seconds [number]', 'Seconds for auto sync', '3600')
+  .parse();
+
+const options = program.opts();
 const startServer = async () => {
-  const server = await initFileSystemServer({ basePath });
+  const server = await initFileSystemServer({
+    basePath: options.basePath,
+    secondsForAutoSync: parseInt(options.seconds),
+    port: parseInt(options.port),
+    startAutoSyncProcess: !options['disableAutoSync'],
+  });
   server.start();
 };
 
