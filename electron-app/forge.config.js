3fb47e2bbfeed1485564f16e9f9b2ed4f975d2e4
Roger Boixader Güell <rboixaderg@gmail.com>
2024-02-27 16:24:35 +0100

    chore: ios release

--- electron-app/forge.config.js
+++ electron-app/forge.config.js
@@ -4,6 +4,19 @@ module.exports = {
     icon: 'public/logo.*',
     name: 'Nuclia sync',
     executableName: 'nuclia-sync-agent',
+    osxSign: {
+      entitlements: 'entitlements.plist',
+      'entitlements-inherit': 'entitlements.plist',
+      'gatekeeper-assess': false,
+      hardenedRuntime: true,
+      identity: 'Developer ID Application: BOSUTECH XXI SL (DF2C2RHNCR)',
+    },
+    osxNotarize: {
+      tool: 'notarytool',
+      appleApiKey: process.env.APPLE_API_KEY,
+      appleApiKeyId: process.env.APPLE_API_KEY_ID,
+      appleApiIssuer: process.env.APPLE_API_ISSUER,
+    },
   },
   rebuildConfig: {},
   makers: [
