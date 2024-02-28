4f0201e694da87957373723106f7c5aa5815b508
Eric BREHAULT <ebrehault@gmail.com>
2024-02-28 08:26:45 +0100

    allow to get logs for a sync and since a date (#19)

--- server/src/logic/logs/domain/use-cases/get-logs.use-case.ts
+++ server/src/logic/logs/domain/use-cases/get-logs.use-case.ts
@@ -2,13 +2,13 @@ import { LogEntity } from '../log.entity';
 import { ILogRepository } from '../log.repository';
 
 export interface GetLogUseCase {
-  execute(): Promise<LogEntity[]>;
+  execute(sync?: string): Promise<LogEntity[]>;
 }
 
 export class GetLogs implements GetLogUseCase {
   constructor(private readonly repository: ILogRepository) {}
 
-  execute(): Promise<LogEntity[]> {
-    return this.repository.getLogs();
+  execute(sync?: string, since?: string): Promise<LogEntity[]> {
+    return this.repository.getLogs(sync, since);
   }
 }
