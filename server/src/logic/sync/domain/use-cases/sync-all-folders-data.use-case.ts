abf4cde2ac8f231057465612bf14d0de2efc4ea1
Eric BREHAULT <ebrehault@gmail.com>
2024-03-06 19:37:24 +0100

    Ericbrehault/sc 9191/do not overflow nuclia with sitemap connector (#24)
    * create resou
    rces in serie, not parallel
    
    * fix the sync command
    
    * lint

--- server/src/logic/sync/domain/use-cases/sync-all-folders-data.use-case.ts
+++ server/src/logic/sync/domain/use-cases/sync-all-folders-data.use-case.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { delay, forkJoin, map, Observable, of, switchMap, tap } from 'rxjs';
+import { concatMap, delay, lastValueFrom, map, of, switchMap, tap, toArray } from 'rxjs';
 
 import { EVENTS } from '../../../../events/events';
 import { eventEmitter } from '../../../../server';
-import { FileStatus } from '../../../connector/domain/connector';
+import { FileStatus, SyncItem } from '../../../connector/domain/connector';
 import { UpdateSyncDto } from '../dto/update-sync.dto';
 import { SyncEntity } from '../sync.entity';
 import { ISyncRepository } from '../sync.repository';
@@ -43,69 +43,65 @@ export class SyncAllFolders implements SyncAllFoldersUseCase {
     new UpdateSync(this.repository).execute(updateSyncDto!);
   };
 
+  processSyncEntity(syncEntity: SyncEntity) {
+    return syncEntity.getLastModified().pipe(
+      map((result) => {
+        return { result, syncEntity };
+      }),
+      switchMap(({ result, syncEntity }) => {
+        eventEmitter.emit(EVENTS.START_SYNCHRONIZATION_SYNC_OBJECT, {
+          from: syncEntity.id,
+          to: syncEntity.kb?.knowledgeBox || 'Unknown kb',
+          date: new Date().toISOString(),
+          total: result.results?.length || 0,
+        });
+
+        if (!result.success || result.results.length === 0) {
+          this.callbackFinishSync(syncEntity, [], 0, result.error);
+          return of(undefined);
+        }
+        return this.processItems(syncEntity, result.results);
+      }),
+    );
+  }
+
+  processItems(syncEntity: SyncEntity, items: SyncItem[]) {
+    return of(...items).pipe(
+      concatMap((item) =>
+        new SyncSingleFile(syncEntity, item).execute().pipe(
+          map((res) => ({ id: item.originalId, success: res.success })),
+          // do not overwhelm the source
+          delay(500),
+        ),
+      ),
+      toArray(),
+    );
+  }
+
   async execute() {
     const syncObjects = await this.repository.getAllSync();
-    of(Object.values(syncObjects))
-      .pipe(
-        switchMap((syncObjectValues) => {
-          if (syncObjectValues.length === 0) {
-            return of(undefined);
-          } else {
-            return forkJoin(
-              syncObjectValues.map((syncObj) =>
-                of(syncObj).pipe(
-                  switchMap((syncObj) => new RefreshAccessToken(this.repository).execute(new SyncEntity(syncObj))),
-                  switchMap((syncEntity) =>
-                    syncEntity.getLastModified().pipe(
-                      map((result) => {
-                        return { result, syncEntity };
-                      }),
-                    ),
-                  ),
-                  switchMap(({ result, syncEntity }) => {
-                    eventEmitter.emit(EVENTS.START_SYNCHRONIZATION_SYNC_OBJECT, {
-                      from: syncEntity.id,
-                      to: syncEntity.kb?.knowledgeBox || 'Unknown kb',
-                      date: new Date().toISOString(),
-                      total: result.results?.length || 0,
-                    });
+    const syncObjectValues = Object.values(syncObjects);
+    if (syncObjectValues.length > 0) {
+      await lastValueFrom(
+        of(...syncObjectValues).pipe(
+          switchMap((syncObj) => new RefreshAccessToken(this.repository).execute(new SyncEntity(syncObj))),
+          switchMap((syncEntity) =>
+            this.processSyncEntity(syncEntity).pipe(
+              tap((result) => {
+                if (result) {
+                  const processed = result.map((res) => res.id);
+                  const successCount = result.filter((res) => res.success).length;
 
-                    if (!result.success || result.results.length === 0) {
-                      this.callbackFinishSync(syncEntity, [], 0, result.error);
-                      return of(undefined);
-                    }
-
-                    const batch: Observable<{ id: string; success: boolean }>[] = result.results.map((item) =>
-                      of(item).pipe(
-                        switchMap((item) =>
-                          new SyncSingleFile(syncEntity, item).execute().pipe(
-                            map((res) => ({ id: item.originalId, success: res.success })),
-                            // do not overwhelm the source
-                            delay(500),
-                          ),
-                        ),
-                      ),
-                    );
-
-                    return forkJoin(batch).pipe(
-                      tap((result) => {
-                        if (result) {
-                          const processed = result.map((res) => res.id);
-                          const successCount = result.filter((res) => res.success).length;
-
-                          console.log('processed', processed);
-                          console.log('successCount', successCount);
-                          this.callbackFinishSync(syncEntity, processed, successCount, '');
-                        }
-                      }),
-                    );
-                  }),
-                ),
-              ),
-            );
-          }
-        }),
-      )
-      .subscribe(() => console.log('Finish sync folders data'));
+                  console.log('processed', processed);
+                  console.log('successCount', successCount);
+                  this.callbackFinishSync(syncEntity, processed, successCount, '');
+                }
+              }),
+            ),
+          ),
+        ),
+      );
+      console.log('Finish sync folders data');
+    }
   }
 }
