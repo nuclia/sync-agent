225d30e3d34f48773cfd746989f09b39e97aa845
Eric BREHAULT <ebrehault@gmail.com>
2024-03-13 15:11:43 +0100

    fix unit tests

--- server/tests/sever.spec.js
+++ server/tests/sever.spec.js
@@ -9,9 +9,9 @@ describe('Basic server', () => {
   });
 
   serverTest('Get sync - empty folder', async ({ server }) => {
-    const response = await request(server.app).get('/sync');
+    const response = await request(server.app).get('/sync/kb/test');
     expect(response.status).toBe(200);
-    expect(Object.keys(response.body).length).toEqual(0);
+    expect(response.body.length).toEqual(0);
   });
 
   serverTest('Get empty logs', async ({ server }) => {
@@ -21,8 +21,8 @@ describe('Basic server', () => {
   });
 
   serverTest('Get connectors', async ({ server }) => {
-    const response = await request(server.app).get('/sync');
+    const response = await request(server.app).get('/sync/kb/test');
     expect(response.status).toBe(200);
-    expect(Object.keys(response.body).length).toEqual(0);
+    expect(response.body.length).toEqual(0);
   });
 });
